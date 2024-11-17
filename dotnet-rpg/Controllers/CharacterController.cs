67cd5eb543f3d0446e20f949135df097ef5f6169
jacoder <mantle@vivaldi.net>
2024-11-17 06:56:52 -0500

    ATTRIBUTE ROUTING
    Adding routing attributes to api methods to prevent ambigious method
    calls and specify how to get to each api method.

--- dotnet-rpg/Controllers/CharacterController.cs
+++ dotnet-rpg/Controllers/CharacterController.cs
@@ -11,11 +11,31 @@ namespace dotnet_rpg.Controllers
     public class CharacterController : ControllerBase
     {
         private static Character knigth = new Character();
+        private static List<Character> characters = new List<Character>{
+            new Character(),
+            new Character{
+                Name = "Sam",
+                HitPoints = 150
+                }
+        };
 
         [HttpGet]
         public ActionResult<Character> Get()
         {
             return Ok(knigth);
+        }
+
+        [HttpGet]
+        [Route("GetallCharacters")]
+        public ActionResult<List<Character>> GetAll()
+        {
+            return Ok(characters);
+        }
+
+        [HttpGet("getfirst")]
+        public ActionResult<Character> GetFirstCharacter()
+        {
+            return characters[0];
         }
     }
 }
